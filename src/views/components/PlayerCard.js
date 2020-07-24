77d1ddede57a29909113358d9c538758c8fd3385
Joonas Malinen <joonas.malinen@vedia.fi>
2020-07-24 15:01:18 +0300

    Fixes to styling ft Höpis

--- src/views/components/PlayerCard.js
+++ src/views/components/PlayerCard.js
@@ -4,23 +4,17 @@ import { styles } from '../styles';
 import { Button } from './Button';
 
 export function PlayerCard({ text, lifeAction, style, player, playerCount }) {
-    let cardStyle = styles.lifeContainer2
-    if (parseInt(player.substr(1)) % 2) {
-        cardStyle = styles.lifeContainer1
-    }
     return (
-        <View style={cardStyle}>
-            <View style={{ flex: 1 }}>
-                <Button
-                    action={() => { lifeAction(player, 1) }}
-                    label='+'
-                />
-                <Text style={styles.life}>{text}</Text>
-                <Button
-                    action={() => { lifeAction(player, -1) }}
-                    label='-'
-                />
-            </View>
+        <View style={style}>
+            <Button
+                action={() => { lifeAction(player, 1) }}
+                label='+'
+            />
+            <Text style={styles.life1}>{text}</Text>
+            <Button
+                action={() => { lifeAction(player, -1) }}
+                label='-'
+            />
         </View>
     )
 }
--- src/views/styles.js
+++ src/views/styles.js
@@ -3,28 +3,36 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        flexDirection: 'column',
+        flexDirection: 'row',
     },
-    life: {
+    leftContainer: {
+        flexGrow: 1,
+        backgroundColor: 'red',
+    },
+    rightContainer: {
+        backgroundColor: 'blue',
+        flexGrow: 1,
+    },
+    life1: {
         fontSize: 40,
+        transform: [{ rotate: '90deg' }],
+        alignSelf: 'center'
+    },
+    life2: {
+        fontSize: 40,
+        transform: [{ rotate: '270deg' }],
         alignSelf: 'center'
     },
     lifeContainer1: {
         flex: 1,
         flexDirection: 'column',
-        paddingLeft: 100,
         fontSize: 40,
         alignSelf: 'center',
-        // transform: [{ rotate: '90deg' }],
-        paddingBottom: 100,
     },
     lifeContainer2: {
         flex: 1,
         flexDirection: 'column',
-        marginTop: 20,
         fontSize: 40,
         alignSelf: 'center',
-        // transform: [{ rotate: '270deg' }],
     },
 })
